d92b0aa7b2a6626afada200155ef5f82483fe69e
Rakshit Gautam <rakshitgautam01@gmail.com>
2021-04-27 22:43:35 +0530

    layout management and added title

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from "react";
 import {  Switch, Route } from 'react-router-dom'
-import Navs from "./Components/Navs";
 import Home from './Pages/Home'
 import Starred from './Pages/Starred'
 
 function App() {
   return (
-    <div>
-      <Navs />
-      <Switch>
-        <Route exact path="/"><Home /></Route>
-        <Route exact path="/starred"><Starred /></Route>
-        <Route>This is 404 error page</Route>
-      </Switch>
-    </div>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+
+      <Route exact path="/starred">
+        <Starred />
+      </Route>
+
+      <Route>
+        Page Not Found
+      </Route>
+    </Switch>
   );
 }
 
--- src/Pages/Starred.js
+++ src/Pages/Starred.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import MainPageLayout from '../Components/MainPageLayout'
 
 const Starred = () => {
     return (
-        <div>
+        <MainPageLayout>
             This is starred page
-        </div>
+        </MainPageLayout>
     )
 }
 
