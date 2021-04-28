6fe6a3e3e41c2c2682bfa82f22ec6aa2f0017996
Rakshit Gautam <rakshitgautam01@gmail.com>
2021-04-28 11:35:30 +0530

    created preview card

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-console */
 import React, {useState} from 'react'
+import ActorGrid from '../Components/Actor/ActorGrid';
+import ShowGrid from '../Components/Show/ShowGrid';
 import MainPageLayout from '../Components/MainPageLayout'
 import {apiGet} from '../Misc/config'
 
@@ -41,16 +43,12 @@ const Home = () => {
         }
 
         if(results && results.length > 0){
-            return results[0].show ? 
-            results.map(item => (
-            <div key={item.show.id}>{item.show.name}</div>
-            )) :
-            results.map(item => (
-            <div key={item.person.id}>{item.person.name}</div>
-            ))
-  
+            return results[0].show ? (
+                <ShowGrid data={results} />
+            ) : (
+                <ActorGrid data={results}/>
+            );
         }
-
         return null;
     }
 
