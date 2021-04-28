fbeef63893175bb399813f26ad28fdb5129ff3dd
Rakshit Gautam <rakshitgautam01@gmail.com>
2021-04-28 16:54:28 +0530

    added loading and error msgs

--- src/Pages/Show.js
+++ src/Pages/Show.js
@@ -7,15 +7,29 @@ const Show = () => {
     
     const {id} = useParams();
     const [show, setShow] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(()=>{
         apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
-            setShow(results);
+
+            setTimeout(()=>{
+                setShow(results);
+                setIsLoading(false);
+            }, 2000)          
+        }).catch(err => {
+            setError(err.msg);
+            setIsLoading(false)
         })
     }, [id])
     console.log('show',show);
 
-
+    if(isLoading){
+        return <div>data is loading</div>
+    }
+    if(error){
+        return <div>Error Occured: {error}</div>
+    }
     return (<div>this is show poge</div>
     )
 }
